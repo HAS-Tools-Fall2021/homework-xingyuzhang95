f65d1f1d302e37a0ae5fc375fe32bc22f44c60d5
xingyuzhang95 <xingyuzhang@email.arizona.edu>
2021-10-18 01:03:44 -0700

    update hw

--- Forecast_Submissions/Code_Review1/XiZhang_homework 7.py
+++ Forecast_Submissions/Code_Review1/XiZhang_homework 7.py
@@ -54,21 +54,32 @@ fig.savefig("p1_time_series_daily.png")
 # %% 
 #2. Time series of flow values with the x axis range limited
 fig, ax = plt.subplots()
-ax.plot(data['datetime'], data['flow'], label='weekly')
-ax.set(title="Observed Flow", xlabel="Date", ylabel="Weekly Avg Flow [cfs]",
-        yscale='log', xlim=[datetime.date(2014, 1, 26), datetime.date(2020, 10, 1)])
+Oct_data = data[(data['month'] == 10) & (data['day'] >= 17) & (data['day'] <= 23) 
+        & (data['year'] <= 2020) ].groupby('year').mean()
+Oct_4th_data = data[(data['month'] == 10) & (data['day'] >= 23) & (data['day'] <= 31) 
+        & (data['year'] <= 2020) ].groupby('year').mean()
+ax.plot(Oct_data.index, Oct_data['flow'], label='3rd week')
+ax.plot(Oct_data.index, Oct_4th_data['flow'], label='4th week')
+ax.set(title="Observed Flow for Oct 17th to 23rd", xlabel="yeaar", ylabel="Weekly Avg Flow [cfs]",
+        yscale='log', )
+ax.text(1995, 380, 'week 3 mean = '+str(round(np.mean(Oct_data['flow']),3))\
+        , fontsize=12)
+ax.text(1995, 320, 'week 4 mean = '+str(round(np.mean(Oct_4th_data['flow']),3))\
+        , fontsize=12)
 ax.legend()
 plt.show()
 fig.savefig("p2_time_series_2014.png")
 # %% 
 #3 Boxplot of flows by month 
 def generate_boxplot(variable_x,variable_y):
+    # Here i'm separating out the x lable and ylable 
+    # setting just as an illustration
+    # They also could have been included in the ax.set command above
     fig, ax = plt.subplots()
     ax = sns.boxplot(x=variable_x, y=variable_y,  data=data,orient='h',\
                     linewidth=0.3)
     ax.set(xscale='log')
-    # Here i'm separating out the x lable and ylable setting just as an illustration
-    # They also could have been included in the ax.set command above
+    
     ax.set_xlabel(variable_x)
     ax.set_ylabel(variable_y)
     return fig
